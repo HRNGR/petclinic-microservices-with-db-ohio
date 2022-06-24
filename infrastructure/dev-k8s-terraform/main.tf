d45c6c24061d9267ac4714889422303135a339b9
HRNGR <gurharun@gmail.com>
2022-06-24 19:14:50 +0000

    added dev-k8s-terraform  for kubernetes infrastructure

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -134,12 +134,12 @@ resource "aws_security_group" "matt-kube-master-sg" {
 
 resource "aws_instance" "kube-master" {
     ami = "ami-013f17f36f8b1fefb"
-    instance_type = "t2.medium"
+    instance_type = "t3a.medium"
     iam_instance_profile = module.iam.master_profile_name
     vpc_security_group_ids = [aws_security_group.matt-kube-master-sg.id, aws_security_group.matt-kube-mutual-sg.id]
     key_name = "mattkey"
-    subnet_id = "subnet-c41ba589"  # select own subnet_id of us-east-1a
-    availability_zone = "us-east-1a"
+    subnet_id = "subnet-0ce0121e2b9bb040e"  # select own subnet_id of us-east-2a
+    availability_zone = "us-east-2a"
     tags = {
         Name = "kube-master"
         "kubernetes.io/cluster/mattsCluster" = "owned"
@@ -152,12 +152,12 @@ resource "aws_instance" "kube-master" {
 
 resource "aws_instance" "worker-1" {
     ami = "ami-013f17f36f8b1fefb"
-    instance_type = "t2.medium"
+    instance_type = "t3a.medium"
         iam_instance_profile = module.iam.worker_profile_name
     vpc_security_group_ids = [aws_security_group.matt-kube-worker-sg.id, aws_security_group.matt-kube-mutual-sg.id]
     key_name = "mattkey"
-    subnet_id = "subnet-c41ba589"  # select own subnet_id of us-east-1a
-    availability_zone = "us-east-1a"
+    subnet_id = "subnet-0ce0121e2b9bb040e"  # select own subnet_id of us-east-2a
+    availability_zone = "us-east-2a"
     tags = {
         Name = "worker-1"
         "kubernetes.io/cluster/mattsCluster" = "owned"
@@ -170,12 +170,12 @@ resource "aws_instance" "worker-1" {
 
 resource "aws_instance" "worker-2" {
     ami = "ami-013f17f36f8b1fefb"
-    instance_type = "t2.medium"
+    instance_type = "t3a.medium"
     iam_instance_profile = module.iam.worker_profile_name
     vpc_security_group_ids = [aws_security_group.matt-kube-worker-sg.id, aws_security_group.matt-kube-mutual-sg.id]
     key_name = "mattkey"
-    subnet_id = "subnet-c41ba589"  # select own subnet_id of us-east-1a
-    availability_zone = "us-east-1a"
+    subnet_id = "subnet-0ce0121e2b9bb040e"  # select own subnet_id of us-east-2a
+    availability_zone = "us-east-2a"
     tags = {
         Name = "worker-2"
         "kubernetes.io/cluster/mattsCluster" = "owned"
